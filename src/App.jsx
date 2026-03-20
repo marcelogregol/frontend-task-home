a40327e9958f9e3ba824be4a199b35504824f716
MarceloGregol <46580730+MarceloGregol@users.noreply.github.com>
2026-03-20 17:28:48 +0000

    Protect private routes and redirect guests to login

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 /* pages */
 import Login from "./components/pages/Auth/Login";
@@ -13,6 +13,15 @@ import UpdateTeam from "./components/pages/Team/UpdateTeam";
 import ListTeam from "./components/pages/Team/ListTeam";
 import UpdateTask from "./components/pages/Task/UpdateTask";
 
+function ProtectedRoute({ children }) {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
 
 function App() {
 
@@ -23,13 +32,13 @@ function App() {
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/editteam/:id" element={<UpdateTeam />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/listteam" element={<ListTeam />} />
-            <Route path="/task" element={<Task />} />
-            <Route path="/updatetask/:id" element={<UpdateTask />} />
+            <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+            <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+            <Route path="/editteam/:id" element={<ProtectedRoute><UpdateTeam /></ProtectedRoute>} />
+            <Route path="/team" element={<ProtectedRoute><Team /></ProtectedRoute>} />
+            <Route path="/listteam" element={<ProtectedRoute><ListTeam /></ProtectedRoute>} />
+            <Route path="/task" element={<ProtectedRoute><Task /></ProtectedRoute>} />
+            <Route path="/updatetask/:id" element={<ProtectedRoute><UpdateTask /></ProtectedRoute>} />
           </Routes>
         </UserProvider>
       </Router>
